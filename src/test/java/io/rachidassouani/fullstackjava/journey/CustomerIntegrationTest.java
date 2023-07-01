09a0272930c7b4282e2c9ef711f51152bd820541
rachidassouani <rachidassouani@gmail.com>
2023-07-01 13:27:56 +0100

    Refactored integration test for registartion customer function

--- src/test/java/io/rachidassouani/fullstackjava/journey/CustomerIntegrationTest.java
+++ src/test/java/io/rachidassouani/fullstackjava/journey/CustomerIntegrationTest.java
@@ -1,14 +1,19 @@
 package io.rachidassouani.fullstackjava.journey;
 
 import com.github.javafaker.Faker;
+import io.rachidassouani.fullstackjava.customer.Customer;
 import io.rachidassouani.fullstackjava.customer.CustomerRegistrationRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -18,6 +23,9 @@ public class CustomerIntegrationTest {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    private static final String CUSTOMER_URI = "api/v1/customers";
+
 
     @Test
     void registerCustomerTest() {
@@ -34,11 +42,52 @@ public class CustomerIntegrationTest {
 
         // send post request
         webTestClient.post()
-                .uri("api/v1/customers")
+                .uri(CUSTOMER_URI)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(request), CustomerRegistrationRequest.class)
                 .exchange()
                 .expectStatus().isOk();
+
+        // get all customers
+        List<Customer> allCustomers = webTestClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {})
+                .returnResult()
+                .getResponseBody();
+
+        // make sure that the customer is present
+        Customer expectedCustomer = new Customer(firstName, lastName, email);
+
+        assertThat(allCustomers)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .contains(expectedCustomer);
+
+        // get the id
+        var id = allCustomers.stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(c -> c.getId())
+                .findFirst()
+                .orElseThrow();
+
+        expectedCustomer.setId(id);
+
+        // get customer by id
+        Customer foundedCustomer = webTestClient.get()
+                .uri(CUSTOMER_URI + "/{id}",  id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<Customer>() {})
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(foundedCustomer.getId()).isEqualTo(expectedCustomer.getId());
+        assertThat(foundedCustomer.getFirstName()).isEqualTo(expectedCustomer.getFirstName());
+        assertThat(foundedCustomer.getLastName()).isEqualTo(expectedCustomer.getLastName());
+        assertThat(foundedCustomer.getEmail()).isEqualTo(expectedCustomer.getEmail());
     }
 }
