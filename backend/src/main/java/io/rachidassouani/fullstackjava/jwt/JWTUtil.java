2638bfde21d2c2e8c43c7a8bfbf3019d02fb6bca
rachidassouani <rachidassouani@gmail.com>
2023-07-31 20:23:46 +0100

    Added issueToken method in order generate a jwt token

---
+++ backend/src/main/java/io/rachidassouani/fullstackjava/jwt/JWTUtil.java
@@ -0,0 +1,45 @@
+package io.rachidassouani.fullstackjava.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+@Service
+public class JWTUtil {
+
+    private final static String SECRET_KEY =
+            "rachidassouani.io_123456789_rachidassouani.io_123456789_rachidassouani.io_123456789";
+
+    public String issueToken(String subject, Map<String, Object> claims) {
+        String token = Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuer("https://rachidassouani.io")
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(1, DAYS)))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+        return token;
+    }
+
+    public String issueToken(String subject) {
+        return issueToken(subject, Map.of());
+    }
+
+    public String issueToken(String subject, String ...scopes) {
+        return issueToken(subject, Map.of("scopes", scopes));
+    }
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+}
