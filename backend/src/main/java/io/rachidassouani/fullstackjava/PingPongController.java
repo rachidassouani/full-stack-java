66f136b7baac8d2556e20889ae90075da99a7a9e
rachidassouani <rachidassouani@gmail.com>
2023-08-08 17:00:40 +0100

    Added ping controller and maked it to be public, so no authentication needed to access it

---
+++ backend/src/main/java/io/rachidassouani/fullstackjava/PingPongController.java
@@ -0,0 +1,17 @@
+package io.rachidassouani.fullstackjava;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    private static int COUNTER = 0;
+
+    record PingPong(String result) { }
+
+    @GetMapping("ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong: %s".formatted(++COUNTER));
+    }
+}
--- backend/src/main/java/io/rachidassouani/fullstackjava/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/io/rachidassouani/fullstackjava/security/SecurityFilterChainConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
@@ -33,6 +34,8 @@ public class SecurityFilterChainConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
+                .authorizeHttpRequests(auth ->  auth
+                        .requestMatchers(GET, "/ping").permitAll())
                 .authorizeHttpRequests(auth ->  auth
                         .requestMatchers(POST, "/api/v1/customers/**", "/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated())
